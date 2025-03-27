c017078009a09989298fa26e05f33956a5e2987b
07picjul <bongus@fungusmail.com>
2025-03-27 12:07:20 +0100

    Change06_J

--- src/main.cpp
+++ src/main.cpp
@@ -23,22 +23,42 @@
 // ****************************************************************************
 
 #include <Arduino.h>
+#include "helper.h"
+#include "analogblinker.h"
 
 #define LED1 A1   // Low-side switch (leuchtet mit LOW)
 #define LED2 A2   // Low-side switch (leuchtet mit LOW)
 #define TASTER1 6 // LOW wenn gedrückt
 #define TASTER2 5 // LOW wenn gedrückt
-#define POT1 A7
+#define POTI A7
+
+#define DB_ZEIT 50
+#define BLINKTIME 300
+#define LONGPRESSTIME 1000
+
+#define ANALOGBLINKER_POLLTIME 10 // ms
+#define ANALOGBLINKER_MIN 100     // ms
+#define ANALOGBLINKER_MAX 3000    // ms
+
+// Instanzieren der Strukturen
+button taster1;
+
+analogblinker a_blinker;
 
 void setup()
 {
     Serial.begin(115200); // Baud rate
     Serial.println("..Start..\n");
 
-    
+    taster1.init(TASTER1, false, DB_ZEIT, LONGPRESSTIME);
+    a_blinker.init(LED1, LED2, BLINKTIME, ANALOGBLINKER_POLLTIME, true, true);
 }
 
 void loop()
 {
+    taster1.poll();
+    a_blinker.poll();
+
+    a_blinker.blinkTime = ((((ANALOGBLINKER_MAX - ANALOGBLINKER_MIN) / 1023.0) * analogRead(POTI)) + 100) / 4;
 
 }
